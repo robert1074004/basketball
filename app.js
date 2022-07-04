9a904608f74df3b87aff595c0fb3b72de6298141
robert1074004 <abc50113@yahoo.com.tw>
2022-07-04 20:35:18 +0800

    feat: add bootstrap & fontawesome and modify main.hbs

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 
 app.set('view engine','hbs')
 
+app.use(express.static('public'))
+
 app.get('/',(req,res) => {
     res.render('index')
 })
