748c4d47fdabbabfa240053677f671ee524fd4f4
robert1074004 <abc50113@yahoo.com.tw>
2022-07-23 22:37:05 +0800

    feat: modify register function & add flash messages

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
-
+const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 
@@ -12,9 +15,16 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static('public'))
 
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false}))
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+})
 app.use(routes)
 
 
-app.listen(3000,() => {
-    console.log('App is running on http://localhost:3000')
+app.listen(port,() => {
+    console.log(`App is running on http://localhost:${port}`)
 })
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,21 +2,33 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
 const userController = {
-    registerPage: (req, res) => {
-        res.render('register')
-    },
-    register: (req, res) => {
-        bcrypt.hash(req.body.password,10)
-          .then(hash => User.create({
-            name: req.body.name,
-            email: req.body.email,
-            position: req.body.position,
-            password: hash
-          }))
-          .then(() => {
-            res.redirect('/login')
-          })
-    }
+  registerPage: (req, res) => {
+    res.render('register')
+  },
+  register: (req, res, next) => {
+    const { name, email, password, confirmPassword, position } = req.body
+    if (password !== confirmPassword) throw new Error('Password do not match!')
+    User.findOne({
+        where: {
+          email
+        }
+      })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash => User.create({
+        name,
+        email,
+        position,
+        password: hash
+      }))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號!')
+        res.redirect('/login')
+      })
+      .catch(err => next(err))
+  }
 }
 
 module.exports = userController
