63921624ae6f3ad937721c7b92c35843dca342cd
robert1074004 <abc50113@yahoo.com.tw>
2023-05-14 10:05:09 +0800

    feat: modify app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const app = express()
-const port = process.env.PORT || 3000
-const SESSION_SECRET =  process.env.SECRET
+const port = process.env.PORT || 8080
+
 
 app.use(methodOverride('_method'))
 app.engine('hbs', exphbs.engine({defaultLayout:'main',extname:'.hbs',helpers: handlebarHelpres}))
@@ -24,7 +24,7 @@ app.use('/upload',express.static(path.join(__dirname, 'upload')))
 app.use(express.urlencoded({ extended: true }))
 
 
-app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false, cookie:{_expires : 86400000}}))
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: false, cookie:{_expires : 86400000}}))
 
 app.use(passport.initialize())
 app.use(passport.session())
