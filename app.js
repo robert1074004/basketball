1475260ce908338875e2d09435dfd7f41b9da381
robert1074004 <abc50113@yahoo.com.tw>
2023-05-30 19:59:03 +0800

    feat: prepare for google app deploy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 
 app.use(methodOverride('_method'))
