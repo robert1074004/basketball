024fff0a808c2cca216cf6618bf3226f678a702b
robert1074004 <abc50113@yahoo.com.tw>
2023-12-10 19:39:17 +0800

    feat: modify recordValidator

--- middleware/validator.js
+++ middleware/validator.js
@@ -17,7 +17,7 @@ const recordValidator = (req, res, next) => {
     req.body.efg = advanceData.getEfg(FGM,THREE_PM,FGA)
     req.body.ts = advanceData.getTs(PTS,FTA,FGA)
     req.body.to_v = advanceData.getTov(TOV,FTA,FGA)
-    if (req.body.efg > 100 || req.body.ts> 100 || req.body.to_v > 100) throw new Error('Some fields were wrong !')
+    if ( req.body.to_v > 100) throw new Error('Some fields were wrong !')
     next()
 }
    
